ad831bd50ce7b3d5862aafed59e533e5d19e9f0f
fc2gmxnet <fc2@gmx.net>
2025-04-16 08:46:59 -0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -32,7 +32,7 @@ st.title(titulo_inci)
 if st.button('???'):
     translations = f"{st.session_state.row['EN']} | {st.session_state.row['BR']} | {st.session_state.row['ES']}"
     st.title(translations)
-    st.header(st.session_state.row['Info'])
+    st.title(st.session_state.row['Info'])
     st.title(st.session_state.row['IUPAC'])
     
     # Display image
