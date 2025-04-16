dc27958d8358acaf9c25bcffcc77554df414eeaf
fc2gmxnet <fc2@gmx.net>
2025-04-16 08:25:17 -0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -5,66 +5,31 @@ import pandas as pd     # For working with dataframes (tables)
 import random           # For working with randomness
 
 # Read the Excel file into a DataFrame
-df_original = pd.read_excel("https://raw.githubusercontent.com/fc2gmxnet/chino/main/INCI.xlsx", engine='openpyxl')
+data = pd.read_excel("https://raw.githubusercontent.com/fc2gmxnet/chino/main/HPC_INCI.xlsx", engine='openpyxl')
 
 
-# Page configuration
-st.set_page_config(
-    page_title='Google',
-    page_icon=':magnifying_glass_tilted_left:',
-    layout='wide'
-)
+# Function to select a random row
+def get_random_row():
+    return data.sample(1).iloc[0]
 
-st.write('INCI')
+# Initialize session state
+if 'row' not in st.session_state:
+    st.session_state.row = get_random_row()
 
-toggle = st.toggle("Start with description")
+# Display title (INCI)
+st.title(st.session_state.row['INCI'])
 
-if toggle:
-    columna_pregunta = 1
-    columna_respuesta = 0
-else:
-    columna_pregunta = 0
-    columna_respuesta = 1
-
-######
-
-# Dropdown (selectbox) with three options
-option = st.selectbox(
-    "Select:",
-    ("Ingredient", "Function", "Formulation type")
-)
-
-# Assign different values to 'variable' based on the selection
-if option == "Ingredient":
-    variable = 'ingredient'
-elif option == "Function":
-    variable = 'function'
-elif option == "Formulation type":
-    variable = 'formulation type'
-
-df = df_original.loc[df_original.Category == variable]
-
-######
-
-# Initialize or update the session state to store the random row index
-if "random_index" not in st.session_state:
-    st.session_state.random_index = random.randint(0, len(df) - 1)
-
-# Function to pick a new random row
-def get_new_random_row():
-    st.session_state.random_index = random.randint(0, len(df) - 1)
-
-# Get the current random row
-random_row = df.iloc[st.session_state.random_index]
-
-# Display the value of the question
-st.title(random_row.iloc[columna_pregunta])
-
-# Button to reveal values of the answer
+# Button to reveal translations and info
 if st.button('???'):
-    st.title(random_row.iloc[columna_respuesta])
+    translations = f"{st.session_state.row['EN']} | {st.session_state.row['BR']} | {st.session_state.row['ES']}"
+    st.title(translations)
+    st.title(st.session_state.row['Info'])
+    
+    # Display image
+    image_url = f"https://raw.githubusercontent.com/fc2gmxnet/chino/imagenes/{st.session_state.row['Picture']}"
+    st.image(image_url)
 
-# Button to select a new random row
-if st.button(" +++ "):
-    get_new_random_row()
-    st.rerun()  # Refresh the app to display the new random row
+# Button to get a new random row
+if st.button('+++'):
+    st.session_state.row = get_random_row()
+    st.rerun()
