8cca5eac9ded19293c43cef90ccf564b53410b6f
fc2gmxnet <fc2@gmx.net>
2026-01-22 14:24:05 -0300

    Update mp3_player.py

--- mp3_player.py
+++ mp3_player.py
@@ -12,7 +12,10 @@ mp3_files = {
     "Sales management": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/sales_management.mp3",
     "Boss management": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Boss-Management-UK.mp3",
     "Six Sigma Pricing": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Six_Sigma_Pricing.mp3",
-    "Music Jazz French": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Music_Space_Jazz_Afternoon_French.mp3"
+    "Music Jazz French": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Music_Space_Jazz_Afternoon_French.mp3",
+    "Music Jazz Nature 1": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Music_Space_Jazz_Nature_1.mp3",
+    "Music Jazz Nature 2": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Music_Space_Jazz_Nature_2.mp3",
+    "Music Jazz Nature 3": "https://raw.githubusercontent.com/fc2gmxnet/chino/main/mp3/Music_Space_Jazz_Nature_3.mp3",
     # Add more as needed
 }
 
