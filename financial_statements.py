b965a0df42b5e05d5cc7c44a0017f6e108e83a0b
fc2gmxnet <fc2@gmx.net>
2026-02-20 21:52:30 -0300

    Update financial_statements.py

--- financial_statements.py
+++ financial_statements.py
@@ -134,8 +134,6 @@ shortcut = """
 # Use components.html for cleaner JS execution or stick to markdown
 st.components.v1.html(shortcut, height=0)
 
-
-
 # 7. Image below buttons
 #if columna_imagen in current_row.index and not pd.isna(current_row[columna_imagen]):
  #   st.image(
